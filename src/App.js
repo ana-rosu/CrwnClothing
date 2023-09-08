7c13c18fd32df22da4bad00d4f99ae047f137ac7
Ana Roșu <anarosu766@gmail.com>
2023-09-08 15:14:52 +0300

    added signIn component & firebase utils

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
+import SignIn from "./routes/signin/signin.component";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="/shop" element={<h1>Hei</h1>} />
+        <Route path="/signin" element={<SignIn />} />
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -17,6 +17,9 @@ const Navigation = () => {
           <Link className="nav-link" to="/shop">
             Shop
           </Link>
+          <Link className="nav-link" to="/signin">
+            Sign In
+          </Link>
         </div>
       </div>
       <Outlet />
