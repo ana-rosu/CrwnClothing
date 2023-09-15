55faedc23d88ff920ec5a20b35fc8c349a81e88b
Ana Roșu <anarosu766@gmail.com>
2023-09-15 16:31:01 +0300

    added routes for homepage directory of categories

--- src/components/DirectoryItem/directory-item.component.jsx
+++ src/components/DirectoryItem/directory-item.component.jsx
@@ -1,18 +1,22 @@
-import "./directory-item.styles.scss";
+import { useNavigate } from "react-router-dom";
+import {
+  BackgroundImage,
+  DirectoryItemBody,
+  DirectoryItemContainer,
+} from "./directory-item.styles";
 
 const DirectoryItem = ({ category }) => {
-  const { title, imageUrl } = category;
+  const { imageUrl, title, route } = category;
+  const navigate = useNavigate();
+  const onNavigateHandler = () => navigate(route);
   return (
-    <div className="directory-item-container">
-      <div
-        className="background-image"
-        style={{ backgroundImage: `url(${imageUrl})` }}
-      />
-      <div className="directory-item-body">
+    <DirectoryItemContainer onClick={onNavigateHandler}>
+      <BackgroundImage imageUrl={imageUrl} />
+      <DirectoryItemBody>
         <h2>{title}</h2>
-        <p>Shop now</p>
-      </div>
-    </div>
+        <p>Shop Now</p>
+      </DirectoryItemBody>
+    </DirectoryItemContainer>
   );
 };
 
